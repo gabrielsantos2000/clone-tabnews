47862abc43fab63e48240f4cd7e3dc4c522ef7e6
Gabriel Oliveira <30851944+gabrielsantos2000@users.noreply.github.com>
2024-12-08 22:58:44 +0000

    Ajustando mensagenm

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Uma mensagem especial, para uma pessoa especial!</h1>
+    return <h1>Mim te ama muito, nenéca, peteca, sapeca. Quer morar e casar comigo? Hein? Hein?👉👈</h1>
 }
 
 export default Home;
