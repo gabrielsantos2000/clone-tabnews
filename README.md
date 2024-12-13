c273c23b8c8b1865742104f5bb94e8d70f9be977
Gabriel Oliveira <30851944+gabrielsantos2000@users.noreply.github.com>
2024-12-13 02:37:22 +0000

    adicionando scripts  e `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do site tabnews para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mim te ama muito, nenéca, peteca, sapeca. Quer morar e casar comigo? Hein? Hein?👉👈</h1>
+  return (
+    <h1>
+      Mim te ama muito, nenéca, peteca, sapeca. Quer morar e casar comigo? Hein?
+      Hein?👉👈
+    </h1>
+  );
 }
 
 export default Home;
